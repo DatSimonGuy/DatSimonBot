b66117039e5161704f1ac985ccab06109da867da
DatSimonGuy <szymon23koszalka@gmail.com>
2024-09-04 19:29:26 +0200

    Add image handling to the database

--- dsbMain/modules/stable/database.py
+++ dsbMain/modules/stable/database.py
@@ -24,6 +24,14 @@ class Database(Module):
         self._logger.log(f"Data saved to {subdir}/{filename}")
 
     @run_only
+    def save_image(self, data: bytes, subdir: str, filename: str) -> None:
+        """ Save image to the database. """
+        os.makedirs(f"{self._directory}/{subdir}", exist_ok=True)
+        with open(f"{self._directory}/{subdir}/{filename}.png", "wb") as file:
+            file.write(data)
+        self._logger.log(f"Image saved to {subdir}/{filename}")
+
+    @run_only
     def load(self, subdir: str, filename: str) -> dict:
         """ Load data from the database. """
         try:
@@ -36,6 +44,18 @@ class Database(Module):
             return {}
 
     @run_only
+    def load_image(self, subdir: str, filename: str) -> bytes:
+        """ Load image from the database. """
+        try:
+            with open(f"{self._directory}/{subdir}/{filename}.png", "rb") as file:
+                data = file.read()
+            self._logger.log(f"Image loaded from {subdir}/{filename}")
+            return data
+        except FileNotFoundError:
+            self._logger.log(f"File {subdir}/{filename} not found")
+            return b""
+
+    @run_only
     def delete(self, subdir: str, filename: str) -> None:
         """ Delete data from the database. """
         try:
@@ -43,6 +63,17 @@ class Database(Module):
             if not os.listdir(f"{self._directory}/{subdir}"):
                 os.rmdir(f"{self._directory}/{subdir}")
             self._logger.log(f"Data deleted from {subdir}/{filename}")
+        except FileNotFoundError:
+            self._logger.log(f"File {subdir}/{filename} not found")
+
+    @run_only
+    def delete_image(self, subdir: str, filename: str) -> None:
+        """ Delete image from the database. """
+        try:
+            os.unlink(f"{self._directory}/{subdir}/{filename}.png")
+            if not os.listdir(f"{self._directory}/{subdir}"):
+                os.rmdir(f"{self._directory}/{subdir}")
+            self._logger.log(f"Image deleted from {subdir}/{filename}")
         except FileNotFoundError:
             self._logger.log(f"File {subdir}/{filename} not found")
 
