b3a65bb9d82f2568d79403ed069f0f4dc59e8e09
Soja <62205459+DatSimonGuy@users.noreply.github.com>
2024-09-19 18:02:15 +0200

    Improve readablility of help command and minor fixes (#20)
    Co-authored-by: DatSimonGuy <62205459+Lazerpl12@users.noreply.github.com>

--- dsb_main/modules/stable/telebot_modules/help.py
+++ dsb_main/modules/stable/telebot_modules/help.py
@@ -1,6 +1,7 @@
 """ Telebot help module """
 
 from telegram import Update
+from telegram.ext import ContextTypes
 from .base.base_module import BaseModule
 
 class Help(BaseModule):
@@ -14,9 +15,17 @@ class Help(BaseModule):
             "help": "Display help message"
         }
 
-    async def _help(self, update: Update, _) -> None:
+    async def _help(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """ Display help message """
-        help_message = "Available commands:\n"
-        for command, desc in self._telebot_module.commands.items():
-            help_message += f"{command} - {desc}\n"
-        await update.message.reply_text(help_message)
+        args, kwargs = self._get_args(context)
+        if "botfather" in kwargs or "botfather" in args:
+            help_message = "BotFather format commands:\n```\n"
+            for command, desc in self._telebot_module.commands.items():
+                help_message += f"{command} - {desc}\n"
+            help_message += "```"
+            await update.message.reply_text(help_message, parse_mode="Markdown")
+        else:
+            help_message = "Available commands:\n"
+            for command, desc in self._telebot_module.commands.items():
+                help_message += f"/{command} - {desc}\n"
+            await update.message.reply_text(help_message)
