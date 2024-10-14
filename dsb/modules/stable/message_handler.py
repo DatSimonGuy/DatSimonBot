b9e9bf5916fe565aa8713e1b78e06fa9998031dc
Soja <62205459+DatSimonGuy@users.noreply.github.com>
2024-10-14 15:44:03 +0200

    Add ability to transfer plans to another user (#36)
    * Add option to transfer ownership of a plan
    
    * Fix user info method and add handler properly
    
    * Remove unnecessary info from command
    
    * Better use of markdown
    
    * Add missing description

--- dsb/modules/stable/message_handler.py
+++ dsb/modules/stable/message_handler.py
@@ -9,22 +9,36 @@ class MessageHandler(BaseModule):
     """ Module for handling text messages """
     def __init__(self, ptb, telebot) -> None:
         super().__init__(ptb, telebot)
+        self._handlers = {
+            "who_am_i": self._user_info,
+        }
+        self._descriptions = {
+            "who_am_i": "Get user id",
+        }
         self._messages = {}
         self._message_handler = telegram.ext.MessageHandler(filters.ALL & ~filters.COMMAND,
                               self._handle_text)
 
     def add_handlers(self) -> None:
         """ Add handlers """
+        super().add_handlers()
         self._bot.add_handler(self._message_handler)
 
     def remove_handlers(self) -> None:
         """ Remove handlers """
+        super().remove_handlers()
         self._bot.remove_handler(self._message_handler)
 
     @property
     def messages(self) -> dict:
         """ Returns the list of messages """
         return self._messages
+
+    @prevent_edited
+    async def _user_info(self, update: Update, _) -> None:
+        """ Get user info """
+        id_info = f"```user_id:\n{update.message.from_user.id}\n```"
+        await update.message.reply_text(f"{id_info}", parse_mode="Markdownv2")
 
     @prevent_edited
     async def _handle_text(self, update: Update, _) -> None:
