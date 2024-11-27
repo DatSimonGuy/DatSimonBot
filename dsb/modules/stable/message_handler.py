b582d9133dd44412ac3f41ebdcf95e538c184762
Soja <62205459+DatSimonGuy@users.noreply.github.com>
2024-11-27 13:57:48 +0100

    57 extend message handler functionality (#66)
    * Add snap feature
    
    * Add a functionality to display logs
    
    * Add missing await statement
    
    * Fix admin checking
    
    * Add missing string conversion
    
    * Remove the correct message
    
    * Fix bot breaking on stickers
    
    * Split emote handling to separate functions
    
    * Add a slight delay
    
    * Add quick poll option
    
    * Remove the pin
    
    * Handle long messages
    
    * Prevent errors on messages without text
    
    * Fix help message
    
    * Add another emote and add special cypher encoder
    
    * Add a way to specify which symbols to use
    
    * Fix bad order
    
    * Fix bad order
    
    * Change the current implementation
    
    * Fix invalid char assigment
    
    * Rename the function
    
    * Britishn't
    
    * Change how the cipher works
    
    * Add missing description
    
    * Remove unused variables
    
    ---------
    
    Co-authored-by: DatSimonGuy <62205459+Lazerpl12@users.noreply.github.com>

--- dsb/modules/stable/message_handler.py
+++ dsb/modules/stable/message_handler.py
@@ -1,9 +1,10 @@
 """ Module for handling text messages """
 
+import asyncio
 from telegram import Update
-from telegram.ext import filters
+from telegram.ext import filters, ContextTypes
 import telegram.ext
-from dsb.types.module import BaseModule, prevent_edited
+from dsb.types.module import BaseModule, prevent_edited, admin_only
 
 class MessageHandler(BaseModule):
     """ Module for handling text messages """
@@ -11,9 +12,20 @@ class MessageHandler(BaseModule):
         super().__init__(ptb, telebot)
         self._handlers = {
             "who_am_i": self._user_info,
+            "whoami": self._user_info,
+            "what_broke": self._what_broke,
+            "silly_cipher": self._silly_cipher
         }
         self._descriptions = {
             "who_am_i": "Get user id",
+            "whoami": "Get user id (alias)",
+            "what_broke": "Get last log message",
+            "silly_cipher": "Decode or encode from silly language"
+        }
+        self._handled_emotes = {
+            "🫰": self._snap,
+            "📊": self._ynpoll,
+            "➕": self._repeat
         }
         self._messages = {}
         self._message_handler = telegram.ext.MessageHandler(filters.ALL & ~filters.COMMAND,
@@ -34,6 +46,16 @@ class MessageHandler(BaseModule):
         """ Returns the list of messages """
         return self._messages
 
+    @admin_only
+    @prevent_edited
+    async def _what_broke(self, update: Update, _) -> None:
+        """ Get last log message """
+        if not self._dsb.logs:
+            await update.message.reply_text("No logs available")
+            return
+        message = self._dsb.logs[-1]
+        await update.message.reply_text(f"```{message}```", parse_mode="Markdownv2")
+
     @prevent_edited
     async def _user_info(self, update: Update, _) -> None:
         """ Get user info """
@@ -41,8 +63,73 @@ class MessageHandler(BaseModule):
         await update.message.reply_text(f"{id_info}", parse_mode="Markdownv2")
 
     @prevent_edited
-    async def _handle_text(self, update: Update, _) -> None:
+    async def _silly_cipher(self, update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+        """ Decode from silly language """
+        big_qwerty = "QWERTYUIOPASDFG HJKLZXCVBNM"
+        qwerty = "qwertyuiopasdfg hjklzxcvbnm"
+        symbols1 = '1234567890@#$_&-+()/*"' + "'" + ':;!?'
+        symbols2 = r'~`|•√π÷×§∆£¢€¥^°={}\%©®™✓[]'
+        if not update.message.reply_to_message:
+            return
+        if not update.message.reply_to_message.text:
+            return
+        text = list(update.message.reply_to_message.text)
+        for i, char in enumerate(text):
+            if char in qwerty:
+                text[i] = symbols1[qwerty.index(char)]
+            elif char in big_qwerty:
+                text[i] = symbols2[big_qwerty.index(char)]
+            elif char in symbols1:
+                text[i] = qwerty[symbols1.index(char)]
+            elif char in symbols2:
+                text[i] = big_qwerty[symbols2.index(char)]
+        text = "".join(text)
+        await update.message.reply_text(text)
+
+    async def _snap(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """ Send a message """
+        user = update.message.from_user
+        user_data = await context.bot.get_chat_member(update.message.chat_id, user.id)
+        is_admin = user_data.status in ["creator", "administrator"]
+        if not str(user.id) in self._dsb.config.get("admins", []) and not is_admin:
+            return
+        if update.message.reply_to_message:
+            await asyncio.sleep(1)
+            to_delete = update.message.reply_to_message.message_id
+            await context.bot.delete_message(chat_id=update.message.chat_id,
+                                                message_id=to_delete)
+            return
+
+    async def _ynpoll(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """ Send a yes/no poll """
+        if not update.message.reply_to_message:
+            return
+        question = update.message.reply_to_message.text
+        await context.bot.send_poll(update.message.chat_id, question,
+                                           ["Yes", "No"], is_anonymous=False)
+
+    async def _repeat(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """ Repeat a message """
+        if not update.message.reply_to_message:
+            return
+        if not update.message.reply_to_message.text:
+            return
+        await context.bot.send_message(update.message.chat_id,
+                                        update.message.reply_to_message.text)
+
+    async def _nerd_detection(self, update: Update, _) -> None:
+        """ Detect nerds """
+        await update.message.set_reaction("🤓")
+
+    @prevent_edited
+    async def _handle_text(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """ Handle text messages """
+        if update.message.text in self._handled_emotes:
+            await self._handled_emotes[update.message.text](update, context)
+            return
+        if update.message.text and len(update.message.text) > 200:
+            await self._nerd_detection(update, context)
+            return
         if update.message.chat_id not in self._messages:
             self._messages[update.message.chat_id] = [update.message]
         else:
