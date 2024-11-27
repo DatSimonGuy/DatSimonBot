1effd32128e5dd2e92d3f4febfc9537aae439c25
Soja <62205459+DatSimonGuy@users.noreply.github.com>
2024-11-27 13:39:15 +0100

    Fix missing comma

--- dsb/modules/stable/planner.py
+++ dsb/modules/stable/planner.py
@@ -106,7 +106,7 @@ class Planner(BaseModule):
             "edit_plan": "Edit a plan name",
             "status": "Get status of all students in the group",
             "where_next": "Send room you have lesson in next",
-            "where_now": "Send room you have your lesson in now"
+            "where_now": "Send room you have your lesson in now",
             "join_plan": "Join a lesson plan",
             "leave_plan": "Leave a lesson plan",
             "get_students": "Get all students in a plan",
