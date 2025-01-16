2faa15bb221dec4dc3e006961926f51ed5fefd8e
DatSimonGuy <szymon23koszalka@gmail.com>
2025-01-16 12:04:23 +0100

    Fix pylint errors

--- dsb/modules/stable/daily_images.py
+++ dsb/modules/stable/daily_images.py
@@ -101,7 +101,7 @@ class DailyImages(BaseModule):
             image_set = kwargs["set"]
         if not image_set:
             sets = context.chat_data.get("sets", None)
-            sets = [x for x in sets.keys()]
+            sets = list(sets.keys())
             await update.message.reply_text("Avaible sets:\n" + "\n".join(sets))
             return
         context.bot_data["daily_images"].update({update.effective_chat.id: image_set})
@@ -180,7 +180,7 @@ class DailyImages(BaseModule):
             image_set = kwargs["set"]
         if not image_set:
             sets = context.chat_data.get("sets", None)
-            sets = [x for x in sets.keys()]
+            sets = list(sets.keys())
             await update.message.reply_text("Avaible sets:\n" + "\n".join(sets))
             return
         image = self._get_image(context.chat_data["sets"][image_set])
