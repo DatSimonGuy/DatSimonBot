41e9805b9d324990ef93912af08aac4f2f206d1b
DatSimonGuy <szymon23koszalka@gmail.com>
2025-04-26 14:48:45 +0200

    Add haiku detection

--- dsb/modules/stable/message_handler.py
+++ dsb/modules/stable/message_handler.py
@@ -6,6 +6,7 @@ import random
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import filters, ContextTypes
 import telegram.ext
+import pronouncing
 from pydub import AudioSegment
 import speech_recognition as sr
 from dsb.types.module import BaseModule, prevent_edited, admin_only
@@ -20,14 +21,16 @@ class MessageHandler(BaseModule):
             "whoami": self._user_info,
             "what_broke": self._what_broke,
             "stt": self._stt,
-            "silly_cipher": self._silly_cipher
+            "silly_cipher": self._silly_cipher,
+            "random_haiku": self._get_random_haiku
         }
         self._descriptions = {
             "who_am_i": "Get user id",
             "whoami": "Get user id (alias)",
             "what_broke": "Get last log message",
             "silly_cipher": "Decode or encode from silly language",
-            "stt": "Transcribe voice message"
+            "stt": "Transcribe voice message",
+            "random_haiku": "Get a random haiku from the chat"
         }
         self._handled_emotes = {
             "🫰": self._snap,
@@ -264,12 +267,73 @@ class MessageHandler(BaseModule):
             if os.path.exists(flac_path):
                 os.remove(flac_path)
 
+    def __detect_haikus(self, message: str) -> str | None:
+        """ Detect haikus in a message """
+        words = message.split()
+        def count_syllables(word: str):
+            if word in r".,:;'\/?()+=-_[]{}*&^%$#@!~`":
+                return 0
+            phones = pronouncing.phones_for_word(word.lower())
+            if not phones:
+                return 1
+            return pronouncing.syllable_count(phones[0])
+        syllables = [count_syllables(word) for word in words]
+        lines = [[], [], []]
+        lines_length = [5, 7, 5]
+        current_line = 0
+        for i, s in enumerate(syllables):
+            if lines_length[current_line] - s > 0:
+                lines[current_line].append(words[i])
+                lines_length[current_line] -= s
+            elif lines_length[current_line] - s == 0:
+                lines[current_line].append(words[i])
+                current_line += 1
+                if current_line > 2 and i != len(words) - 1:
+                    return None
+                elif current_line > 2:
+                    return "\n".join(" ".join(line) for line in lines)
+            else:
+                return None
+        return None
+
+    @prevent_edited
+    async def _get_random_haiku(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """
+        Sends a random haiku from the current chat
+        
+        Usage: /random_haiku
+        
+        """
+        haikus: dict = context.chat_data.get("haikus", None)
+        if haikus is None:
+            await update.message.reply_text("No one has made any haikus yet")
+            return
+        random_user = random.choice(list(haikus.keys()))
+        random_haiku = random.choice(haikus[random_user])
+        await update.message.reply_text(random_haiku)
+
     @prevent_edited
     async def _handle_text(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """ Handle text messages """
-        if update.message.text in self._handled_emotes:
+        if update.message.text and update.message.text in self._handled_emotes:
             await self._handled_emotes[update.message.text](update, context)
             return
+        if update.message.text is not None:
+            detected_haikus = self.__detect_haikus(update.message.text)
+            if detected_haikus is not None:
+                haiku_message = await update.message.reply_text(detected_haikus +
+                                                f"\n\n- {update.effective_user.username}")
+                haikus: dict = context.chat_data.get("haikus", None)
+                user = update.effective_user.username
+                if haikus is not None:
+                    user_haikus = haikus.get(user, None)
+                    if user_haikus is None:
+                        haikus[user] = [haiku_message.text]
+                    else:
+                        user_haikus.append(haiku_message.text)
+                else:
+                    context.chat_data["haikus"] = {user: [haiku_message.text]}
+                return
         if update.message.text and len(update.message.text) > 200:
             await self._nerd_detection(update, context)
             return
